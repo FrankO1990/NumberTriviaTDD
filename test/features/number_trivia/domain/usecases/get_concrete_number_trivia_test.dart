2a68fa656a130a94d997df937865157f3ebf9c4b
Frank Ohlhof <frank.ohlhof@yucon-digital.de>
2021-12-08 11:17:16 +0000

    add use case contract, add tests for concrete and random number trivia and add implementation

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,14 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:number_trivia/core/error/failures.dart';
+import 'package:mockito/annotations.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'get_concrete_number_trivia_test.mocks.dart';
 
-class MockNumberTriviaRepository extends Mock
-    implements NumberTriviaRepository {}
-
+// class MockNumberTriviaRepository extends Mock
+//     implements NumberTriviaRepository {}
+@GenerateMocks([NumberTriviaRepository])
 void main() {
   // added late keyword to make setUp function
   // initialization pass the null safety compiler errors
@@ -30,7 +31,7 @@ void main() {
       when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
           .thenAnswer((_) async => Right(tNumberTrivia));
       // Act
-      final result = await usecase.execute(number: tNumber);
+      final result = await usecase(Params(number: tNumber));
       // Assert
       expect(result, Right(tNumberTrivia));
       verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
